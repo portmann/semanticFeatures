7bf29bf6a6ce1c262c5703d04ffd87ba4f58e716
portmann <portmann.samu@gmail.com>
2016-08-31 18:10:14 +0200

    some calcs

--- src/ch/lgt/ming/cleanup/Main.java
+++ src/ch/lgt/ming/cleanup/Main.java
@@ -32,7 +32,7 @@ public class Main {
 					higlightedText = htmlStrings.getBeforeTitle() + title + htmlStrings.afterTitle + higlightedText
 							+ htmlStrings.getEnd();
 
-					filehandler.saveStringAsFile("highlighted/Uncertainty_conditionality2/" + i + ".html", higlightedText);
+					filehandler.saveStringAsFile("highlighted/Uncertainty_conditionality_401_499/" + i + ".html", higlightedText);
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
